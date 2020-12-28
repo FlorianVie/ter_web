0d33a5f053c01449dbcbc96e223a1e227e6efa0e
NeonSodium <vieflorian@gmail.com>
2020-12-28 18:31:15 +0100

    init

---
+++ experimentations.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bulma/css/bulma.css">
+    <title>TER</title>
+</head>
+<body>
+<section class="hero is-dark is-fullheight">
+    <div class="hero-body">
+        <div class="container has-text-centered">
+            <h1 class="title">
+                Expérimentations
+            </h1>
+            <div class="columns">
+                <div class="column">
+                    <a href=""><button class="button">Reading span - FR</button></a>
+                    <a href=""><button class="button">Reading span - EN</button></a>
+                </div>
+            </div>
+            <div class="columns">
+                <div class="column">
+                    <button class="button" disabled>Cambridge</button>
+                </div>
+            </div>
+            <div class="columns">
+                <div class="column">
+                    <button class="button" disabled>Retranscription</button>
+                </div>
+            </div>
+            <div class="columns">
+                <div class="column">
+                    <button class="button" disabled>Vitesse de frappe</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
