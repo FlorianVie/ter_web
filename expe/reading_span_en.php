a80d4410913a393dd24fc29d9fe746be42809b02
NeonSodium <vieflorian@gmail.com>
2020-12-31 20:46:25 +0100

    grid

--- expe/reading_span_en.php
+++ expe/reading_span_en.php
@@ -6,12 +6,16 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Reading Span - EN</title>
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap" rel="stylesheet">
+    <script src="jquery.js"></script>
     <script src="jspsych.js"></script>
     <script src="plugins/jspsych-html-keyboard-response.js"></script>
     <script src="plugins/jspsych-html-button-response.js"></script>
     <script src="plugins/recall-plugin.js"></script>
+    <script src="plugins/jspsych-survey-html-form.js"></script>
     <link rel="stylesheet" href="css/jspsych.css">
 </head>
+
 <body></body>
 
 <?php
@@ -23,9 +27,8 @@ $letters = array_column(getLetters($bdd), 'letter');
 
 $tr_nb = 3;
 shuffle($letters);
-print_r($letters);
-
-
+$letters_grid = $letters;
+shuffle($letters_grid);
 ?>
 
 <script>
@@ -61,11 +64,43 @@ print_r($letters);
     }
     ?>
 
+    var btn_clicked = '';
+    var htmlgrid = '<table style="margin: auto" >' +
+        '<tr>' +
+        '<td><button class="jspsych-btn" id="1" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button"><?php echo $letters_grid[0] ?></button></td>' +
+        '<td><button class="jspsych-btn" id="2" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button"><?php echo $letters_grid[1] ?></button></td>' +
+        '<td><button class="jspsych-btn" id="3" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button"><?php echo $letters_grid[2] ?></button></td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td><button class="jspsych-btn" id="1" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button"><?php echo $letters_grid[3] ?></button></td>' +
+        '<td><button class="jspsych-btn" id="2" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button"><?php echo $letters_grid[4] ?></button></td>' +
+        '<td><button class="jspsych-btn" id="3" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button"><?php echo $letters_grid[5] ?></button></td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td><button class="jspsych-btn" id="1" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button"><?php echo $letters_grid[6] ?></button></td>' +
+        '<td><button class="jspsych-btn" id="2" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button"><?php echo $letters_grid[7] ?></button></td>' +
+        '<td><button class="jspsych-btn" id="3" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button"><?php echo $letters_grid[8] ?></button></td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td><button class="jspsych-btn" id="1" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button"><?php echo $letters_grid[9] ?></button></td>' +
+        '<td><button class="jspsych-btn" id="2" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button"><?php echo $letters_grid[10] ?></button></td>' +
+        '<td><button class="jspsych-btn" id="3" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button"><?php echo $letters_grid[11] ?></button></td>' +
+        '</tr>' +
+        '</table>' +
+        '<p><input type="text" name="response" id="resp" style="font-size: 1.3em; text-align: center" ></p>' +
+        '<p><button class="jspsych-btn" id="supp" onclick="btn_clicked =  removeInput()" style="background-color: grey; font-size: 1em" type="button" >Effacer</button></p>'
+
     var training_recall = {
-        type: 'recall-letters',
-        letters_choices: ['R', 'T', 'J', 'K', 'B', 'C', 'S']
+        type: 'survey-html-form',
+        preamble: '<h2>Rappel :</h2>',
+        html: htmlgrid,
+        data: {
+            correct_letters: '<?php for ($i = 0; $i < $tr_nb; $i++) {
+                echo $letters[$i];
+            } ?>'
+        }
     }
-    timeline.push(training_recall)
+    timeline.push(training_recall);
 
     jsPsych.init({
         timeline: timeline,
@@ -74,6 +109,19 @@ print_r($letters);
         }
     })
 
+</script>
+
+<script>
+    function getInputGrid(button, btn_clicked) {
+        btn_clicked += $(button).text();
+        $('#resp').val(btn_clicked);
+        return btn_clicked;
+    }
+
+    function removeInput() {
+        $('#resp').val("");
+        return "";
+    }
 </script>
 
 </html>
--- expe/test.php
+++ expe/test.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Reading Span - EN</title>
+    <script src="jquery.js"></script>
     <script src="jspsych.js"></script>
     <script src="plugins/jspsych-html-keyboard-response.js"></script>
     <script src="plugins/button-response-grid.js"></script>
@@ -40,10 +41,46 @@ shuffle($letters);
     }
     timeline.push(recall);
 
+    function getInputGrid(button, btn_clicked) {
+        btn_clicked += $(button).text();
+        $('#resp').val(btn_clicked);
+        return btn_clicked;
+    }
+
+    function removeInput() {
+        $('#resp').val("")
+    }
+
+    var btn_clicked = '';
+    var htmlgrid = '<table style="margin: auto" >' +
+        '<tr>' +
+        '<td><button class="jspsych-btn" id="1" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button">A</button></td>' +
+        '<td><button class="jspsych-btn" id="2" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button">B</button></td>' +
+        '<td><button class="jspsych-btn" id="3" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button">C</button></td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td><button class="jspsych-btn" id="1" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button">A</button></td>' +
+        '<td><button class="jspsych-btn" id="2" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button">B</button></td>' +
+        '<td><button class="jspsych-btn" id="3" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button">C</button></td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td><button class="jspsych-btn" id="1" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button">A</button></td>' +
+        '<td><button class="jspsych-btn" id="2" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button">B</button></td>' +
+        '<td><button class="jspsych-btn" id="3" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button">C</button></td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td><button class="jspsych-btn" id="1" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button">A</button></td>' +
+        '<td><button class="jspsych-btn" id="2" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button">B</button></td>' +
+        '<td><button class="jspsych-btn" id="3" onclick="btn_clicked = getInputGrid(this, btn_clicked)" type="button">C</button></td>' +
+        '</tr>' +
+        '</table>' +
+        '<p><input type="text" name="response" id="resp" style="font-size: 1.3em; text-align: center" ></p>' +
+        '<p><button class="jspsych-btn" id="supp" onclick="removeInput()" style="background-color: grey; font-size: 1em" type="button" >Effacer</button></p>'
+
     var recall2 = {
         type: 'survey-html-form',
-        preamble: '<p>Rappel :</p>',
-        html: '',
+        preamble: '<h2>Rappel :</h2>',
+        html: htmlgrid,
     }
     timeline.push(recall2);
 
