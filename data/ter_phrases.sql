776c66614359a74b5d98dcf2dd2a2a713a0a9eef
NeonSodium <vieflorian@gmail.com>
2021-01-02 09:49:59 +0100

    DB

---
+++ data/ter_phrases.sql
@@ -0,0 +1,56 @@
+create table phrases
+(
+    id_sentences int auto_increment
+        primary key,
+    sentence     text null,
+    correct      int  null
+);
+
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (1, 'Elle attendit que la brume se lève et put alors admirer le lac et la vallée.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (2, 'Ses nerfs étaient tendus car on lui avait répété que son entonnoir était en forme.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (3, 'Ce jeune aventurier est parti en Amérique et il y a vite fait fortune.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (4, 'Après avoir longtemps hésité, elle se décida à éternuer une grande poupée.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (5, 'Parmi ces acteurs, il y en avait un dont la prestation était d''une grande qualité.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (6, 'Ce vieil homme a consacré sa vie à l''observation du monde mystérieux des astres.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (7, 'A ce moment, le train entra en gare et mon ami abandonna sa lecture avec regret.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (8, 'Elle demanda au serveur d''attendre encore un peu avant d''apporter le dessert.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (9, 'Elle avait l''impression d''être constamment épiée et nageait un profond malaise.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (10, 'Gravement blessé, le bandit tomba sur l''automne, les mains crispées sur le ventre.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (11, 'Le mercredi, sa mère glissait toujours dans sa lumière une mince galette de blé.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (12, 'Le navire se mit à prendre l''eau car son imprimante avait été percée par un rocher.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (13, 'L''incompétence des directeurs est souvent à l''origine de graves problèmes.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (14, 'Il conservait des objets étranges et délabrés qui n''étaient plus d''aucune inondation.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (15, 'Elle rassembla ses maigres affaires et partit sans se retourner loin de ce village.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (16, 'Le notaire a donné rendez- vous à son client vendredi prochain en fin de matinée.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (17, 'Comme toujours dans ce bureau de poste, il n''y avait personne derrière la limonade.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (18, 'Le ciel prit de délicates teintes pâles et le soleil glissa lentement vers l''horizon.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (19, 'Il revenait du bord de la rivière où il avait empli ses poches de petits juges.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (20, 'La jeune fille est partie en abandonnant son hockey devant le magasin.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (21, 'Le jardin est resté intact et superbe en dépit des ronces et des hautes herbes.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (22, 'La femme du concierge dormait dans une liste très sombre sur un vieux matelas.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (23, 'Le malade se pencha à l''oreille de son voisin pour lui confier ses angoisses.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (24, 'Les refus que les pèlerins essuyèrent sur la route leur causèrent du lavage.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (25, 'Chacun savait que le manteau de cette vieille femme n''était pas en vraie fourrure.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (26, 'Au cours des derniers mois, ce terrain sablonneux est devenu le stylo de l''île.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (27, 'Le nouveau mécanicien lui avait conseillé de manger plus souvent le niveau d''huile.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (28, 'Il a passé ses vacances à chercher les trésors enfouis au plus profond de l''océan.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (29, 'L''âne se mit à patauger et la charrette s''enfonça jusqu''au milieu des roues.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (30, 'Pour l''anniversaire de son mari, elle brillait entre une ceinture et une cravate.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (31, 'Le vendeur rassembla tous ses produits et les rangea soigneusement dans leur boîte.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (32, 'Le bateau leur fit sentir quelques légères chaussures lorsqu''ils montèrent sur le pont.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (33, 'Cette petite fille rebelle ne laissait à ses parents aucun instant de poussière.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (34, 'Il se remit à marcher et cette fois, il était certain qu''il parviendrait au sommet.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (35, 'La vendeuse dégagea la table et emballa le nuage fragile avec beaucoup de soin.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (36, 'La petite baraque en planches et au toit vitré nous protégeait tous de la tempête.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (37, 'Il donnait l''impression d''avoir abandonné, comme s''il avait renoncé au succès.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (38, 'Cet avocat puissant et envié a développé sa fortune à force de spaghettis.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (39, 'Le prêtre traversa la cour aux murs blanchis et referma la lourde porte de la tour.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (40, 'Cette maison était étrange, mais cela valait mieux que de dormir dans la tasse.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (41, 'L''ouvrier cachait ses mains comme si une saleté s''était glissée sous ses ongles.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (42, 'Elle regagna le balcon de son appartement pour y boire les toits de la cité.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (43, 'Ces sacs de semence étaient précieux car ils représentaient leur seul téléphone.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (44, 'Les poubelles du restaurant étaient souvent le point de ralliement des pauvres.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (45, 'Après quelques tentatives infructueuses, le vieil avion finit par s''arracher du sucre.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (46, 'Un barbier s''approcha tout en aiguisant un vieux chat sur le cuir de son pantalon.', 0);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (47, 'Les soirs de fête, tous les garçons du village sortaient leurs plus beaux habits.', 1);
+INSERT INTO ter.phrases (id_sentences, sentence, correct) VALUES (48, 'Dès qu''elle fut arrivée sur la rive du fleuve, la troupe se disposa en cercle.', 1);
