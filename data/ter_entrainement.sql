8b544def8c76044639b44bf12b029129e30f8d5e
NeonSodium <vieflorian@gmail.com>
2021-01-02 09:49:32 +0100

    DB

---
+++ data/ter_entrainement.sql
@@ -0,0 +1,23 @@
+create table entrainement
+(
+    id_practice  int auto_increment
+        primary key,
+    pracSentence text null,
+    correct      int  null
+);
+
+INSERT INTO ter.entrainement (id_practice, pracSentence, correct) VALUES (1, 'Un étranger apparut sur le seuil et tendit à la fille un petit sac de fenêtres.', 0);
+INSERT INTO ter.entrainement (id_practice, pracSentence, correct) VALUES (2, 'Malgré tout son courage, la servante ne parvenait pas à accomplir son dur labeur.', 1);
+INSERT INTO ter.entrainement (id_practice, pracSentence, correct) VALUES (3, 'Après avoir dévasté le village, le vieux pirate ordonna de gagner le large.', 1);
+INSERT INTO ter.entrainement (id_practice, pracSentence, correct) VALUES (4, 'Le tonnerre grondait sauvagement sur le chantier et la foudre tomba sur le toit.', 1);
+INSERT INTO ter.entrainement (id_practice, pracSentence, correct) VALUES (5, 'Les gendarmes qui ont perquisitionné les lieux ont trouvé des envies et des armes.', 0);
+INSERT INTO ter.entrainement (id_practice, pracSentence, correct) VALUES (6, 'Devant ce spectacle, même l''homme le plus brave aurait un moment de faiblesse.', 1);
+INSERT INTO ter.entrainement (id_practice, pracSentence, correct) VALUES (7, 'Le marin, qui manifestait tous les signes de la colère, entra dans la cuisine.', 1);
+INSERT INTO ter.entrainement (id_practice, pracSentence, correct) VALUES (8, 'Craignant encore d''être abandonné, le chien suivait son maître comme une pelouse.', 0);
+INSERT INTO ter.entrainement (id_practice, pracSentence, correct) VALUES (9, 'A cet instant, la vitre fut brisée par un souhait qui rebondit sur le tapis.', 0);
+INSERT INTO ter.entrainement (id_practice, pracSentence, correct) VALUES (10, 'Les élèves ne contenaient plus leur impatience lorsqu''approcha l''orange de la leçon.', 0);
+INSERT INTO ter.entrainement (id_practice, pracSentence, correct) VALUES (11, 'Le comptable réunit les derniers dossiers et les porta dans le bureau de son chef.', 1);
+INSERT INTO ter.entrainement (id_practice, pracSentence, correct) VALUES (12, 'Derrière l''angle de la maison, un tas d''amours servait de refuge aux araignées.', 0);
+INSERT INTO ter.entrainement (id_practice, pracSentence, correct) VALUES (13, 'A trois heures précises, le troupeau s''engouffrait sous le portail de la grande ferme.', 1);
+INSERT INTO ter.entrainement (id_practice, pracSentence, correct) VALUES (14, 'Il se fit passer pour un inspecteur en civil et entra avec beaucoup de facilité.', 1);
+INSERT INTO ter.entrainement (id_practice, pracSentence, correct) VALUES (15, 'Le flacon contenait un liquide bleu clair et frais, au tuyau de miel et de banane.', 0);
